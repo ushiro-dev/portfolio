e50fa2429353ad43ccde5d3eea9dd67fb8744600
k-ushiro <k-ushiro@gajumale.co.jp>
2026-06-01 10:32:58 +0900

    refactor: rename tech stack section to skill section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <main class="min-h-screen bg-gray-50">
     <HomeIntroSection />
     <HomeStrengthSection />
-    <HomeTechStackSection />
+    <HomeSkillSection />
     <HomeFeaturedProjectsSection />
     <HomeAboutLinkSection />
     <HomeContactSection />
